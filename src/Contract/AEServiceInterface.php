0fe082143967022397ce3983ee71978660cf0840
Amin56413 <Ellie.guo@goldenf.co>
2022-05-03 15:08:26 +0800

    first commit

---
+++ src/Contract/AEServiceInterface.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\AE\Contract;
+/**
+ *
+ */
+interface AEServiceInterface {
+
+    /**
+     * 遊戲啟動
+     *
+     * @param string $opCode
+     * @param string $account
+     * @param string $gameCode
+     * @param string $language
+     * @param string $backUrl
+     * @param string $oddType
+     * @return mixed
+     */
+    function gameLaunch(string $opCode, string $account, string $gameCode, string $language, string $backUrl = null, string $oddType = null);
+
+    /**
+     * 遊戲詳情
+     * @param string $opCode
+     * @param string $account
+     * @param string $betId
+     * @return mixed
+     */
+    function gameDetail(string $opCode, string $account, string $betId);
+}
+
