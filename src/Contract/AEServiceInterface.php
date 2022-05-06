b3b2ca10a772498d4f40ca7b00ea44df46bb7d43
Amin56413 <Ellie.guo@goldenf.co>
2022-05-06 18:10:14 +0800

    add parameter vendorCode

--- src/Contract/AEServiceInterface.php
+++ src/Contract/AEServiceInterface.php
@@ -13,20 +13,22 @@ interface AEServiceInterface {
      * @param string $account
      * @param string $gameCode
      * @param string $language
+     * @param string $vendorCode
      * @param string $backUrl
      * @param string $oddType
      * @return mixed
      */
-    function gameLaunch(string $opCode, string $account, string $gameCode, string $language, string $backUrl = null, string $oddType = null);
+    function gameLaunch(string $opCode, string $account, string $gameCode, string $language, string $vendorCode, string $backUrl = null, string $oddType = null);
 
     /**
      * 遊戲詳情
      * @param string $opCode
      * @param string $account
      * @param string $betId
+     * @param string $vendorCode
      * @param string $language
      * @return mixed
      */
-    function gameDetail(string $opCode, string $account, string $betId, string $language = 'en');
+    function gameDetail(string $opCode, string $account, string $betId, string $vendorCode, string $language = 'en');
 }
 
